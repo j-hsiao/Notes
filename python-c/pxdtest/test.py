f0a92e1be453282eeb2c169e629911929c4df372
Jason Hsiao <oaishnosaj@gmail.com>
2017-06-06 16:50:20 +0800

    minor changes

--- python-c/pxdtest/test.py
+++ python-c/pxdtest/test.py
@@ -6,6 +6,7 @@ import timeit
 
 
 n = 100
-print timeit.timeit('cintegrate.integrate_f(0, 10, 10000)', 'import cintegrate', number=n)
-print timeit.timeit('integrate.integrate_f(0, 10, 10000)', 'import integrate', number=n)
-print timeit.timeit('tint.integrate_f(0, 10, 10000)', 'import tint', number=n)
+print 'cimport other .so: {}'.format(timeit.timeit('cintegrate.integrate_f(0, 10, 10000)', 'import cintegrate', number=n))
+print 'only py:           {}'.format(timeit.timeit('integrate.integrate_f(0, 10, 10000)', 'import integrate', number=n))
+print 'include method:    {}'.format(timeit.timeit('tint.integrate_f(0, 10, 10000)', 'import tint', number=n))
+print 'include, pyfunc:   {}'.format(timeit.timeit('tint.integrate_ff(0, 10, 10000)', 'import tint', number=n))
--- python-c/pxdtest/tint.pyx
+++ python-c/pxdtest/tint.pyx
@@ -12,5 +12,13 @@ cpdef double integrate_f(double a, double b, int N):
     cdef int i
     dx = (b - a) / N
     for i in xrange(N):
+        s += f(a+i*dx)
+    return s * dx
+
+cpdef double integrate_ff(double a, double b, int N):
+    cdef double s = 0
+    cdef int i
+    dx = (b - a) / N
+    for i in xrange(N):
         s += ff(a+i*dx)
     return s * dx
