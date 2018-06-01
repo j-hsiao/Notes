ad4d0a9bd83d9be12163473527784745ee65ca21
Jason Hsiao <oaishnosaj@gmail.com>
2018-06-01 18:01:06 +0800

    continue up to menu

--- tk/test.py
+++ tk/test.py
@@ -1,40 +1,110 @@
-from tkinter import *
+from PIL import Image, ImageTk
+import tkinter
 from tkinter import ttk
+import traceback
+import cv2
+import base64
 
-def calculate(*args):
-    try:
-        value = float(feet.get())
-        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
-    except ValueError:
-        pass
+
+def callback(*args):
+    for arg in args:
+        print arg.x, arg.y
+        print arg
+        print dir(arg)
+
+def drag(*args):
+    for arg in args:
+        print arg.x, arg.y
+    print 'dragging'
+
+def selected(*args):
+    for arg in args:
+        print [
+            arg.widget.get(_)
+            for _ in arg.widget.curselection()]
+
+def swap(box, *args):
+    box.configure(
+        selectmode = (
+            'browse' if box.configure('selectmode')[-1] == 'extended'
+            else 'extended'))
+    print box.configure('selectmode')[-1]
+
+def close(root, *args):
+    root.destroy()
+
+def step(prog, *args):
+    val = int(prog.configure('value')[-1])
+    mv = int(prog.configure('maximum')[-1]) + 1
+    prog.configure(value = (val + 1) % mv)
+
+def start(prog, *args):
+    prog.start()
+def stop(prog, *args):
+    prog.stop()
+
+if __name__ == '__main__':
+    path = '/home/andy/Downloads/fisheye/testim1.jpg'
+    root = tkinter.Tk()
+    im = cv2.imread(path)
+    pim = ImageTk.PhotoImage(
+        Image.fromarray(im[:,:,::-1])
+    )
+    frame = ttk.Frame(root)
+    frame.grid(column = 0,row = 0)
+    frame.configure(height = 500, width = 500)
+    label = ttk.Label(frame)
+    label.grid(column = 0, row = 0)
+    text = ttk.Label(frame, text = 'hello world!')
+    text.grid(column = 1, row = 1)
+    button = ttk.Button(frame, text = 'push me', command = callback)
+    button.grid(column = 0, row = 1)
+    do = True
     
-root = Tk()
-root.title("Feet to Meters")
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
-
-feet = StringVar()
-meters = StringVar()
-
-feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(W, E))
-
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
-
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
-for child in mainframe.winfo_children():
-    child.grid_configure(padx=5, pady=5)
-
-feet_entry.focus()
-root.bind('<Return>', calculate)
+    boxlist = tkinter.StringVar()
+    boxlist.set('I am a {beautiful person}')
+    # class dat(object):
+    #     def __init__(self, val):
+    #         self._val = val
+    #     def __repr__(self):
+    #         return self._val
+    # boxlist = [dat(_) for _ in ['I', 'am', 'a', 'beautiful person']]
+    box = tkinter.Listbox(frame, height = 3, listvariable = boxlist)
+    scrollbar = ttk.Scrollbar(frame, orient = 'vertical', command = box.yview)
+    scrollbar.grid(column = 1, row = 2, sticky = 'w')
+    box.configure(yscrollcommand = scrollbar.set)
+    box.configure(selectmode = 'browse')
+    box.grid(column = 0,row = 2, sticky = 'e')
+    def wrap(func, target):
+        def wut(*args):
+            func(target, *args)
+        return wut
 
 
-root.mainloop()
+    root.bind('q', wrap(close, root))
+    root.bind('<Alt-Key-x>', callback)
+    root.bind('<Button2-Motion>', drag)
+    box.bind('<<ListboxSelect>>', selected)
+    box.bind('b', wrap(swap, box))
+    prog = ttk.Progressbar(
+        frame, orient = 'horizontal',
+        length = 500, mode = 'determinate',
+        maximum = 5,
+        value = 0)
+    prog.grid(column = 0, row = 999, )
+    # prog.bind('<Double-Button-1>', wrap(step, prog))
+    prog.bind('<Button-1>', wrap(start, prog))
+    prog.bind('<Double-Button-1>', wrap(stop, prog))
+    
 
+    try:
+        label.configure(
+            image = pim
+        )
+    except:
+        traceback.print_exc()
+        do = False
+    if do:
+        root.mainloop()
+    
