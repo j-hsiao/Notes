3cc4acf9375f6af8abd29f226dbdef60859546e7
Hsiao <Jason.Hsiao@acer.com>
2024-09-04 05:33:45 +0100

    Add some defaults for setting screen to prevent blackscreen xfce

--- remotehost/remotedesktop/setscreen.sh
+++ remotehost/remotedesktop/setscreen.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+DEFAULTS=(-s 0 -n -t uint)
 for x in blank-on-ac dpms-on-ac-off dpms-on-ac-sleep; do
-	xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/"${x}" "${@}"
+	xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/"${x}" "${@:-"${DEFAULTS[@]}"}"
 done
