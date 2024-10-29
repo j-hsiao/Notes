fcdfff7e122d2afff57a5519d2eb76467b8e47b6
Jason Hsiao <oaishnosaj@gmail.com>
2024-10-29 18:18:12 +0800

    Updating ncomplete, use less to display choices.

--- bash/scripts/ncomplete.sh
+++ bash/scripts/ncomplete.sh
@@ -1,82 +1,31 @@
 #!/bin/bash
 
-# n is aliased to nothing
-# NOTE: this is not suitable for menu completion
-#   This is because "fake" completion choices are
-#   chosen for displaying to the user.  menu completion
-#   would try to use these fake completion choices which
-#   would give the wrong result.
-# Add n before any commands to use num completion.
-# tab to activate completion.  Whenver completion is
-# activated on an empty basename (ending with a /)
-# list numbered options.
-# End with the number to choose it
-# eg.
-# commandline:          tab result
-# n cd /some/path/      list numbered choices
-# n cd /some/path/5     choose the 5th choice
-# n cd /some/path/p     normal completion anything starting with p
-
-# cache results of listing choices.
-# sometimes listing choices can be somewhat expensive so
-# caching may help improve performance
+# numbering starts from one so position 0 will just be the current
+# search
 numeric_completion_choices=()
 alias n=''
-function num_complete() {
-	local target="${2}"
-	if [[ "${target}" =~ ^[0-9]*$ ]]
-	then
-		target="./${target}"
-	elif [[ "${target}" =~ ^'~/'.* ]]
-	then
-		target="${target/#'~'/"${HOME}"}"
-	elif [[ "${target}" = '~' || "${target}" =~ '~'[0-9]+ ]]
-	then
-		target="${target/#'~'/"${HOME}/"}"
-	fi
+num_complete() {
+	local extra="${2#${numeric_completion_choices[0]}}"
 
-	if [[ "${target}" =~ ^.*/[0-9]+$ && "${#numeric_completion_choices[@]}" -gt 0 && "${numeric_completion_choices[0]}${target##*/}" = "${PWD}${COMP_WORDS[*]}" ]]
+	if [[ "${extra}" != "${2}" && "${extra}" =~ ^[0-9]+$ && "${extra}" -lt "${#numeric_completion_choices[@]}" ]]
 	then
-		COMPREPLY=("${target%/*}/${numeric_completion_choices[${target##*/}]}")
-		return
-	fi
-
-	printf '*\b'
-	if [[ "${target}" =~ ^.*/[0-9]*$ ]]
-	then
-		if [ "${COMP_WORDS[1]}" = cd ]
-		then
-			# special handling for cd: only dirnames
-			readarray -t COMPREPLY < <(compgen -o dirnames "${target%/*}/")
-			# compgen will give the entire path
-			COMPREPLY=("${COMPREPLY[@]##*/}")
-		else
-			# ls only gives the individual file/dirname
-			readarray -t COMPREPLY < <(ls -Ap "${target%/*}/")
-		fi
-		if [ -n "${target##*/}" ]
-		then
-			COMPREPLY=("${target%/*}/${COMPREPLY[${target##*/}-1]}")
-			numeric_completion_choices=()
-		else
-			if [ -z "${NCOMPLETE_NO_CACHE}" ]
-			then
-				numeric_completion_choices=("${PWD}${COMP_WORDS[*]}" "${COMPREPLY[@]}")
-			fi
-			if [ "${#COMPREPLY[@]}" -gt 1 ]
-			then
-				# enumerate choices with 0-padded numbers(so properly sorted)
-				while read target
-				do
-					COMPREPLY[$((10#${target}-1))]="${target} ${COMPREPLY[$((10#${target}-1))]}"
-				done < <(seq -w "${#COMPREPLY[@]}")
-			elif [ "${#COMPREPLY[@]}" -eq 1 ]
-			then
-				COMPREPLY=("${target%/*}/${COMPREPLY[0]}")
-			fi
-		fi
-	else
+		COMPREPLY=("${numeric_completion_choices[extra]}")
 		numeric_completion_choices=()
+	else
+		numeric_completion_choices=("${2}")
+		local tmp
+		readarray -O 1 -t tmp < <(compgen -o filenames -o nospace -f "${2}")
+		if [[ "${#tmp[@]}" -eq 1 ]]
+		then
+			COMPREPLY=("${tmp[0]}")
+		else
+			if [[ "${#tmp[@]}" -gt 1 ]]
+			then
+				printf '%s\n' "${tmp[@]}" | cat -n | column | less
+			fi
+			numeric_completion_choices+=("${tmp[@]}")
+			COMPREPLY=()
+		fi
 	fi
 }
-complete -o bashdefault -o default -o nospace -o filenames -F num_complete n
+complete -o nospace -o filenames -F num_complete n
