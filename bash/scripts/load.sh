5748f1dff8bb14f297d55227a7c58dfe2e094582
jason hsiao <oaishnosaj@gmail.com>
2024-11-03 03:20:52 +0000

    Change loadscripts to just define a function.
    Call the function with script dirs as argument.

--- bash/scripts/load.sh
+++ bash/scripts/load.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-case "${BASH_SOURCE[0]}" in
-	*/*)
-		dname="${BASH_SOURCE[0]%/*}"
-		;;
-	*)
-		dname=.
-		;;
-esac
-for f in "${dname}"/*
-do
-	if [[ "${f##*/}" != "${BASH_SOURCE[0]##*/}" ]]
-	then
-		. "${f}"
-	fi
-done
+# load scripts in the containing directory
+
+if ! declare -f load_dir_scripts &>/dev/null
+then
+	load_dir_scripts()
+	{
+		local dname f
+		for dname in "${@}"
+		do
+			for f in "${dname}"/*
+			do
+				if [[ -f "${f}" ]]
+				then
+					. "${f}"
+				fi
+			done
+		done
+	}
+fi
