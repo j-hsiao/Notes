851fcdc569c717abba899f4290a4e0d109c68eb9
Jason Hsiao <oaishnosaj@gmail.com>
2025-02-11 11:58:32 +0800

    Update makeft, allow comment indentation handling for preprocessor # statements too.

--- editors/vim/makeft.sh
+++ editors/vim/makeft.sh
@@ -8,15 +8,20 @@ then
 fi
 
 settings=(
-	"python ts=4 sts=0 sw=0 expandtab"
-	"cmake ts=4 sts=0 sw=0 noexpandtab"
-	"c ts=2 sts=0 sw=0 noexpandtab cms=//\\ %s"
-	"cpp ts=2 sts=0 sw=0 noexpandtab cms=//\\ %s"
-	"sh ts=4 sts=0 sw=0 noexpandtab comments=b:#"
-	"text ts=2 sts=4 sw=2 noexpandtab"
-	"bash ts=4 sts=0 sw=0 noexpandtab comments=b:#"
-	"cuda ts=2 sts=0 sw=0 noexpandtab cms=//\\ %s"
-	"vim ts=4 sts=0 sw=0 noexpandtab"
+	'python ts=4 sts=0 sw=0 expandtab'
+
+	'cmake ts=4 sts=0 sw=0 noexpandtab'
+
+	'c    ts=2 sts=0 sw=0 noexpandtab cms=//\ %s comments+=:#'
+	'cpp  ts=2 sts=0 sw=0 noexpandtab cms=//\ %s comments+=:#'
+	'cuda ts=2 sts=0 sw=0 noexpandtab cms=//\ %s comments+=:#'
+
+	'sh   ts=4 sts=0 sw=0 noexpandtab comments=b:#'
+	'bash ts=4 sts=0 sw=0 noexpandtab comments=b:#'
+
+	'text ts=2 sts=4 sw=2 noexpandtab'
+
+	'vim ts=4 sts=0 sw=0 noexpandtab'
 )
 
 for info in "${settings[@]}"
