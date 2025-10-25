8db4424fb0b50b585a9047cd590bd3678ce119f3
Jason Hsiao <oaishnosaj@gmail.com>
2025-10-25 20:29:15 +0800

    Fix missing space at ending of PS1.

--- bash/env.sh
+++ bash/env.sh
@@ -266,7 +266,7 @@ setup_bash() {
 
 	PS1script='case "${TERM}" in
 		xterm-color|*-256color)
-			PS1='\''${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$'\''
+			PS1='\''${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '\''
 			;;
 		*)
 			PS1='\''${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '\''
