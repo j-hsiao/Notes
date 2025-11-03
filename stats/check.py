8683dcb200c9442bebbbff156ee8d26c4a76d225
Jason Hsiao <oaishnosaj@gmail.com>
2025-11-03 02:12:00 +0000

    Add notes about linear combinations of samples into a larger sample.

--- stats/check.py
+++ stats/check.py
@@ -36,13 +36,60 @@ def check_merge(lo, hi):
         resultvar = (V1*D1 + V2*D2 + (C1*C2/C)*(X1-X2)**2) / D
         print(f'  vardif : {abs(resultvar - targetvar):.10f}')
 
+        DV = V1*D1 + V2*D2 + C1*X1**2 + C2*X2**2 - ((C1*X1 + C2*X2)**2) / (C1+C2)
+        V = DV/D
+        print(f'  vardif : {abs(V - targetvar):.10f}')
+
+
+def check_lincomb(lo, hi):
+    xi = []
+    xij = []
+    Ci = []
+    Xi = []
+    Vi = [None] * lo
+    Di = [None] * lo
+    for dset in range(lo):
+        Ci.append(random.randint(lo, hi))
+        xij.append([])
+        for i in range(Ci[dset]):
+            xij[dset].append(random.randint(lo, hi))
+        Xi.append(mean(xij[dset]))
+        xi.extend(xij[dset])
+
+    X = mean(xi)
+    C = len(xi)
+    for dof in range(2):
+        V = variance(xi, dof)
+        D = C - dof
+        for dset in range(lo):
+            Vi[dset] = variance(xij[dset], dof)
+            Di[dset] = Ci[dset] - dof
+
+        tX = sum([Ci[i]*Xi[i] for i in range(lo)]) / C
+
+        tV = (
+            sum([Di[i]*Vi[i] for i in range(lo)])
+            + sum([Ci[i]*Xi[i]**2 for i in range(lo)])
+            - C*tX**2
+        )
+        print(f'dof: {dof}')
+        print(f'  meandif: {abs(tX - X):.10f}')
+        print(f'  vardif : {abs(tV/D - V):.10f}')
+
+
+
+
 if __name__ == '__main__':
     import argparse
     p = argparse.ArgumentParser()
     p.add_argument(
         'count', nargs='*', type=int, help='lo [hi], range of items per dataset',
-        default = [10, 20])
+        default = [100, 200])
     args = p.parse_args()
     if len(args.count) < 2:
         args.count.append(args.count[0])
+    print('merge')
     check_merge(*args.count)
+
+    print('linear combination')
+    check_lincomb(*args.count)
