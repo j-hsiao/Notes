f5100be8be0f30e50c3fcb7d915ec310ff2329a3
Jason Hsiao <oaishnosaj@gmail.com>
2025-11-22 20:05:34 +0800

    add audio sharing.

--- sysadmin/machinectl/share.sh
+++ sysadmin/machinectl/share.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-dnames=("${XDG_RUNTIME_DIR}")
-fnames=("${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY}")
+dnames=(
+	"${XDG_RUNTIME_DIR}"
+	${XDG_RUNTIME_DIR}/pulse
+)
+
+wayland="${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY}"
+pulse="${XDG_RUNTIME_DIR}/pulse/native"
+
+fnames=(
+	"${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY}"
+	"${XDG_RUNTIME_DIR}/pulse/native"
+)
 
 username="${1:-${USER}_}"
 
 chmod 770 "${dnames[@]}"
 chmod 775 "${fnames[@]}"
-sudo chown "${USER}:${username}" "${dnames[@]}" "${fnames[@]}"
+chown "${USER}:${username}" "${dnames[@]}" "${fnames[@]}"
 
 
 machinectl shell \
 	--setenv=XDG_SESSION_TYPE \
-	--setenv=WAYLAND_DISPLAY="${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY}" \
+	--setenv=WAYLAND_DISPLAY="${wayland}" \
 	--setenv=XAUTHORITY \
+	--setenv=PULSE_SERVER="unix:${pulse}" \
 	"${username}"@
 
 chmod 700 "${dnames[@]}"
