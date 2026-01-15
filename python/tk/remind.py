80201d68bb814e2aa82d584b2f1dd651fd39ee07
Jason Hsiao <jason.hsiao@acer.com>
2026-01-15 12:02:13 +0800

    Update reminder.
    Add launch command.
    Add reuse argument.
    Add more helpful error messages.
    Change auto-start server to default behavior, change --auto to --noauto

--- python/tk/remind.py
+++ python/tk/remind.py
@@ -60,6 +60,7 @@ class Server(object):
     def __init__(self, args):
         self.persist = args.persist
         self.verbose = args.verbose
+        self.reuse = args.reuse
         self.port = args.port
         self.lock = threading.Lock()
         self.reminders = []
@@ -79,7 +80,9 @@ class Server(object):
 
     def run(self):
         t = threading.Thread(target=self.run_server)
-        t.start()
+        # must be in mainloop for the exit event to be caught in case of
+        # failed binding.
+        self.tk.after(0, t.start)
         self.tk.mainloop()
         t.join()
 
@@ -123,11 +126,12 @@ class Server(object):
     def run_server(self):
         L = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            L.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            if self.reuse:
+                L.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             L.bind(('localhost', self.port))
-            print(f'bound to {L.getsockname()}')
-            sys.stdout.flush()
             L.listen(5)
+            print(f'Server bound to {L.getsockname()}.')
+            sys.stdout.flush()
             wait = None
             while 1:
                 r, w, x = select.select([L], (), (), wait)
@@ -196,10 +200,31 @@ class Server(object):
                 self.running = False
             self.tk.event_generate('<<CheckNotifications>>', when='tail')
 
+def launch(args):
+    with open(os.path.join(os.environ['HOME'], '.reminder'), 'ab') as logf:
+        cmd = [sys.executable, sys.argv[0], '-s']
+        if args.verbose:
+            cmd.append('-v')
+        if args.persist:
+            cmd.append('--persist')
+        p = subprocess.Popen(
+            cmd, bufsize=0,
+            stdout=subprocess.PIPE,
+            stderr=logf.fileno())
+    result = p.stdout.readline().decode('utf-8')
+    if not result.startswith('Server bound to '):
+        raise ValueError('Failed to bind to socket. Already up?')
+    print(result, end='')
+    print('Server pid:', p.pid)
+    return p
 
-COMMANDS = ('exit', 'list', 'cancel')
+
+COMMANDS = ('exit', 'list', 'cancel', 'check')
 def send_command(args, retrying=False):
     """Send client command to server. Start server if it is down."""
+    if args.cmd.lower() == 'launch':
+        launch(args)
+        return
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         try:
@@ -207,27 +232,18 @@ def send_command(args, retrying=False):
         except Exception:
             if retrying:
                 traceback.print_exc()
-            elif args.cmd in COMMANDS or not args.auto:
+            elif args.cmd in COMMANDS or args.noauto:
                 if args.verbose:
                     print('Server not active.')
                 return 1
             else:
-                with open(os.path.join(os.environ['HOME'], '.reminder'), 'ab') as logf:
-                    cmd = [sys.executable, sys.argv[0], '-s']
-                    if args.verbose:
-                        cmd.append('-v')
-                    if args.persist:
-                        cmd.append('--persist')
-                    p = subprocess.Popen(
-                        cmd, bufsize=0,
-                        stdout=subprocess.PIPE,
-                        stderr=logf.fileno())
-                p.stdout.readline()
-                print('Server pid:', p.pid)
+                p = launch(args)
                 send_command(args, True)
         else:
             with s.makefile('w') as wf:
                 if args.cmd in COMMANDS:
+                    if args.cmd == 'check':
+                        args.cmd = 'list'
                     print(args.cmd, file=wf)
                 else:
                     print(parse_time(args.cmd, args.delay).strftime(DATE_FMT), file=wf)
@@ -247,10 +263,11 @@ if __name__ == '__main__':
     p.add_argument('-s', '--server', action='store_true', help='act as the server, otherwise client.')
     p.add_argument('-p', '--port', type=int, default=58008, help='reminder server port.')
     p.add_argument('-d', '--delay', action='store_true', help='the given times are delays.')
+    p.add_argument('-r', '--reuse', action='store_true')
     p.add_argument('-v', '--verbose', action='store_true')
-    p.add_argument('-c', '--check', action='store_true')
-    p.add_argument('-a', '--auto', action='store_true')
-    p.add_argument('--persist', action='store_true', help='Server remains up even if no more notifications.')
+    p.add_argument('-c', '--check', action='store_true', help='just check the time parsing.')
+    p.add_argument('-n', '--noauto', action='store_true', help='do not automatically start the server.')
+    p.add_argument('-P', '--persist', action='store_true', help='Server remains up even if no more notifications.')
     p.add_argument('cmd', nargs='?', help=f'the client command: a time specification (YYYY-mm-dd HH:MM:SS), floats allowed, omissions allowed. or one of {COMMANDS}.')
     p.add_argument('extra', nargs='*', help='remaining extra arguments for command.')
     args = p.parse_args()
@@ -259,7 +276,6 @@ if __name__ == '__main__':
         print('now:', datetime.datetime.now().strftime(DATE_FMT))
         print('tgt:', parse_time(args.cmd, args.delay).strftime(DATE_FMT))
     elif args.server:
-        print('server main thread number:', threading.get_native_id())
         Server(args).run()
     else:
         sys.exit(send_command(args))
