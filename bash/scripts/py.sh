16fa434e24df09ee2012ba9bfb251737eb1ef0d6
Jason Hsiao <jason.hsiao@acer.com>
2026-01-16 11:38:32 +0800

    Update py wrapper.
    Hash potential targets before function definition.
    Check hash database, which is faster than checking executable exists.
    Prefer bash path fixing for cygwin over cygpath extra process.
    Remove the env usage which made python startup maybe 50% slower.

--- bash/scripts/py.sh
+++ bash/scripts/py.sh
@@ -1,37 +1,49 @@
 #!/bin/bash
 
+# hash -t is very fast, checking if the corresponding
+# item is hashed or not.  However, using type, etc
+# is very slow.
+#
+# Also, if a function of the same name exists, hash will
+# succeed, but hash -t WILL ALWAYS FAIL.  However, if
+# the actual command is first hashed, before the function
+# is defined, then everything works ok.
+
 if [[ -d /cygdrive ]] && type -fP py &>/dev/null
 then
-	# cygwin with windows py
+	# hash before function definition so py is hashed properly
+	hash py
 	py()
 	{
-		local paths=() args=("${@}") idxs=() idx=-1
-		# cygwin command substitution is very slow
-		# multiple paths would multiply the slowdown...
-		while ((++idx < ${#args[@]}))
+		local paths=() args=("${@}") idxs=() idx
+		# Correct paths from cygwin to windows.
+		# Try to avoid using process substitution
+		# because so slow.
+		for ((idx=0; idx<${#args[@]}; ++idx))
 		do
-			if [[ "${args[idx]:0:1}" = '/' ]]
+			if [[ "${args[idx]}" = /cygdrive/[a-zA-Z]/* ]]
+			then
+				args[idx]="${args[idx]/#\/cygdrive\/}"
+				args[idx]="${args[idx]/['\/']/:/}"
+			elif [[ "${args[idx]}" = /* ]]
 			then
 				paths+=("${args[idx]}")
 				idxs+=("${idx}")
 			fi
 		done
-
 		if ((${#paths[@]}))
 		then
 			readarray -t paths < <(cygpath -w "${paths[@]}")
-			idx=-1
-			while ((++idx < ${#paths[@]}))
+			for ((idx=0; idx<${#paths[@]}; ++idx))
 			do
 				args[idxs[idx]]="${paths[idx]}"
 			done
 		fi
-
 		# cygwin TZ setting seems to mess up windows python datetime
-		env TZ= command py "${args[@]}"
+		TZ= command py "${args[@]}"
 	}
-
 else
+	hash py python2 python3 python &>/dev/null
 	py()
 	{
 		case "${1}" in
@@ -44,7 +56,7 @@ else
 				command python3 "${@}"
 				;;
 			*)
-				if type python &>/dev/null
+				if hash -t python &>/dev/null
 				then
 					command python "${@}"
 				else
