d3876dcc78769497494a1cee929344b9d21a71d3
Jason Hsiao <oaishnosaj@gmail.com>
2026-02-14 06:16:07 +0800

    Check if target remote already exists under a different name before adding.

--- bash/scripts/g.sh
+++ bash/scripts/g.sh
@@ -2,6 +2,7 @@
 
 g()
 {
+	# Set remote g to the original repo's origin remote
 	local orig="${PWD}"
 	local remote="$(git remote get-url origin)"
 	while [[ -d "${remote}" ]]
@@ -11,9 +12,17 @@ g()
 	done
 	local premote="$(git remote get-url origin --push)"
 	cd "${orig}"
-	git remote add g "${remote}"
-	if [[ "${premote}" != "${remote}" ]]
+
+	local remotes="$(git remote -v)"
+	if [[ "${remotes}" != *${remote}* ]]
 	then
-		git remote set-url g --push "${premote}"
+		git remote add g "${remote}" || return
+		if [[ "${premote}" != "${remote}" ]]
+		then
+			git remote set-url g --push "${premote}"
+		fi
+	else
+		local exist="${remotes%%"${remote}"*}"
+		printf 'Remote target exists:\n%s\n' "${exist##*$'\n'}${remote}"
 	fi
 }
