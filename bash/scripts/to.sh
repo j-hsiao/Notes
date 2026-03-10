f0b3b9f7fc6620652de192fc315be056d716756a
Jason Hsiao <jason.hsiao@acer.com>
2026-03-10 09:25:33 +0800

    Expand on some completion notes and update to.sh

--- bash/scripts/to.sh
+++ bash/scripts/to.sh
@@ -2,11 +2,11 @@
 
 alias to=cd
 
-TO_SHORTCUTS=()
+TO_SHORTCUTS=("${TO_SHORTCUTS[@]}")
 
 function to_() {
 	local resets=() local opt
-	for opt in nullglob extglob
+	for opt in nullglob extglob nocaseglob
 	do
 		[[ "${BASHOPTS}" != *"${opt}"* ]] && resets+=("${opt}")
 	done
@@ -22,24 +22,56 @@ function to_() {
 		chosen="${query##*,}"
 		query="${query%,*}"
 	fi
-
-	COMPREPLY=()
-	for item in "${TO_SHORTCUTS[@]}"
-	do
-		if [[ "${item}" = "${query}"* ]]
-		then
-			COMPREPLY+=("${item#*:}")
-		fi
-	done
-	if [[ -d /cygdrive/ ]]
+	if [[ "${query}" = '~'* ]]
 	then
-		COMPREPLY+=(
-			/cygdrive/"${query}"*/
-			'//wsl$/ubuntu/home/'*/"${query}"*/
-		)
+		query="${query/#'~'/"${HOME}"}"
+	fi
+	COMPREPLY=()
+	if [[ "${query}" != /*  && "${query}" != .* ]]
+	then
+		if [[ oldpwd = "${query,,}"* ]]
+		then
+			COMPREPLY+=('"${OLDPWD}"')
+		fi
+		for item in "${TO_SHORTCUTS[@]}"
+		do
+			if [[ "${item}" = "${query}"* ]]
+			then
+				COMPREPLY+=("${item#*:}")
+			fi
+		done
+		local dname
+		for dname in '/cygdrive/' '/mnt/' "/run/media/${USER}/" '//wsl$/ubuntu/home/'*/
+		do
+			if [[ -d "${dname}" ]]
+			then
+				COMPREPLY+=("${dname%/}/${query}"*/)
+			fi
+		done
 	fi
 	COMPREPLY+=("${query}"*/)
 
-	[[ -n "${chosen}" ]] && COMPREPLY=("${COMPREPLY[chosen]}")
+	if [[ -n "${chosen}" ]]
+	then
+		local pick="${COMPREPLY[chosen-1]}"
+		if [[ "${pick}" != '"'* ]]
+		then
+			printf -v pick '%q' "${pick}"
+		fi
+		COMPREPLY=("${pick}")
+	fi
+
+	if (("${#COMPREPLY[@]}">1))
+	then
+		local i=0
+		if (("${#COMPREPLY[@]}">"${LINES}"))
+		then
+			for ((i=1; i<=${#COMPREPLY[@]}; ++i)); do printf '%2d: %s\n' "${i}" "${COMPREPLY[i-1]}"; done | less
+			COMPREPLY=()
+		else
+			for ((i=1; i<=${#COMPREPLY[@]}; ++i)); do printf '\n%2d: %s' "${i}" "${COMPREPLY[i-1]}"; done
+			COMPREPLY=('' ' ')
+		fi
+	fi
 }
-complete -F to_ -o nospace -o nosort to
+complete -F to_ -o nospace to
