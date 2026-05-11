cc07f8418cc3f9250573d8af2a7081f8af795b44
Jason Hsiao <oaishnosaj@gmail.com>
2026-05-11 09:54:46 +0800

    Modify g behavior to work with a local origin.

--- bash/scripts/g.sh
+++ bash/scripts/g.sh
@@ -3,16 +3,61 @@
 g()
 {
 	# Set remote g to the original repo's origin remote
-	local orig="${PWD}"
+	local verbose=:
+	local start="${PWD}"
+	trap 'trap - RETURN; cd "${start}"' RETURN
+	while (($#))
+	do
+		case "${1}" in
+			-v|--verbose) verbose=;;
+			-h|--help)
+				local msg='g [-v] [repo=${PWD}]
+				Follow the "origin" remote until "origin" either points to a non-local repo
+				or it points to a repo that does not have an "origin" remote.  Set "g" remote
+				to this repo.
+
+				-v: verbose
+				repo: the repo to set the "g" remote
+				'
+				echo "${msg//$'\t'}" >&2
+				return
+				;;
+			*)
+				if [[ -d "${1}" ]]
+				then
+					cd "${1}" || return 1
+				else
+					echo "Unknown argument: $1" >&2
+					return 1
+				fi
+				;;
+		esac
+		shift
+	done
+	local root="${PWD}"
 	local remote
 	remote="$(git remote get-url origin)" || return
 	while [[ -d "${remote}" ]]
 	do
+		${verbose} echo "${PWD} -> ${remote}" >&2
 		cd "${remote}"
-		remote="$(git remote get-url origin)" || return
+		local nxt=
+		if nxt="$(git remote get-url origin 2>/dev/null)"
+		then
+			remote="${nxt}"
+		else
+			remote="${PWD}"
+			${verbose} echo "Current dir: ${PWD}" >&2
+			${verbose} echo "remotes:" >&2
+			${verbose} git remote -v >&2
+			cd "${OLDPWD}"
+			break
+		fi
 	done
 	local premote="$(git remote get-url origin --push)"
-	cd "${orig}"
+	cd "${root}"
+	${verbose} echo "remote: ${remote}" >&2
+	${verbose} echo "push  : ${premote}" >&2
 
 	local remotes="$(git remote -v)"
 	if [[ "${remotes}" != *${remote}* ]]
@@ -24,6 +69,6 @@ g()
 		fi
 	else
 		local exist="${remotes%%"${remote}"*}"
-		printf 'Remote target exists:\n%s\n' "${exist##*$'\n'}${remote}"
+		printf 'Remote target exists:\n%s\n' "${exist##*$'\n'}${remote}" >&2
 	fi
 }
